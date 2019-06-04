c4e04cfe8fcb71bd37b90560772c328b2f01aab6
安正超 <anzhengchao@gmail.com>
2019-06-04 16:28:47 +0800

    Update User.php

--- app/User.php
+++ app/User.php
@@ -52,8 +52,43 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function isAdmin()
+    protected static function boot()
     {
-        return false;
+        parent::boot();
+
+        static::saving(function (User $user) {
+            $user->name = $user->name ?? $user->realname;
+
+            if (Hash::needsRehash($user->password)) {
+                $user->password = \bcrypt($user->password);
+            }
+
+            if ($user->isDirty('is_admin') && !\app()->runningInConsole()) {
+                if (!optional(\auth()->user())->is_admin) {
+                    \abort(403, '非法操作');
+                }
+            }
+        });
+    }
+
+    public function getAvatarAttribute()
+    {
+        return $this->attributes['avatar'] ?? self::DEFAULT_AVATAR;
+    }
+
+    public function filterSearch($query, $keyword)
+    {
+        $keyword = \sprintf('%%%s%%', $keyword);
+
+        return $query->where('name', 'like', $keyword)->orWhere('username', 'like', $keyword);
+    }
+
+    public function toArray()
+    {
+        if (\auth()->check() && \auth()->user()->is($this)) {
+            return parent::toArray();
+        }
+
+        return Arr::except(parent::toArray(), self::SENSITIVE_FIELDS);
     }
 }
