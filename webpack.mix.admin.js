6a1b62c982ab2c68bb1200601e71b4ef59e97caf
安正超 <anzhengchao@gmail.com>
2019-11-19 22:16:17 +0800

    Update webpack.mix.admin.js

--- webpack.mix.admin.js
+++ webpack.mix.admin.js
@@ -13,7 +13,11 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: ['html', 'body'],
+  // 不处理的样式
+  // element-ui, mdi, nprogress
+  whitelistPatternsChildren: [/^el-/, /^v-/, /^mdi/, /^nprogress/, /^vxe/],
 })
 
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
